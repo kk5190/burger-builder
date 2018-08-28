508745b27292aec549483240d183489bc5b1a4c0
kk5190 <kk5190krrish@gmail.com>
2018-08-29 00:36:37 +0530

    update ordersummary only if modal update

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Wrapper from '../../../HOC/Wrapper';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const Modal = (props) => (
-  <Wrapper>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div 
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1':'0'
-      }}
-    >
-    
-      {props.children}
-    </div>
-  </Wrapper>
-);
+class Modal  extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) { 
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Wrapper>
+      <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+      <div 
+        className={classes.Modal}
+        style={{
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? '1':'0'
+        }}
+      >
+        {this.props.children}
+      </div>
+    </Wrapper>
+    );
+  }
+};
 
 export default Modal;
